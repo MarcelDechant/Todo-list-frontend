5307bec8d0ed7f5325542c0ab14380ba0b94ca0c
Marcel Dechant <marcel.dechant@freenet.de>
2024-09-04 16:56:14 +0200

    auth_interceptor.service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+  provideClientHydration(),
+  provideHttpClient(),
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptorService,
+    multi: true
+  }
+  ]
 };
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, HttpClientModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
+
+  constructor(private as:AuthService, private router:Router) { }
+  ngOnInit(): void {
+
+  }
+
+  async login() {
+   
+    try {
+    let resp= await this.as.loginWithUsernameAndPassword(this.username, this.password)
+    console.log(resp)  
+    this.router.navigateByUrl('/todos');
+    } catch (e) {
+      alert('Login fehlgeschlagen!')
+      console.error(e)
+    }
+  }
+
+  
+
+
+
+}
